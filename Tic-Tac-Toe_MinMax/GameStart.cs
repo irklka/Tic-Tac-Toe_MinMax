c98a4b8308fd4eb35cba41ca6be095d09caa0137
kapitan <62106615+irklka@users.noreply.github.com>
2022-05-31 22:46:45 +0400

    Add project files.

---
+++ Tic-Tac-Toe_MinMax/GameStart.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Linq;
+
+namespace Tic_Tac_Toe_MinMax
+{
+    public static class GameStart
+    {
+        static string[] board;
+
+        static void Main(string[] args)
+        {
+            board = new string[9];
+            string turn = "X";
+
+            // დაფის შევსება რიცხვებით
+            for (int i = 0; i < 9; ++i)
+                board[i] = (i + 1).ToString();
+
+            PrintBoard();
+            Console.WriteLine("Game Started!");
+
+            while (true)
+            {
+                int numInput;
+                Console.WriteLine("Your Turn, Enter Number {you are 'X'}: ");
+
+                while (true)
+                {
+                    numInput = Int32.Parse(Console.ReadLine()[0].ToString());
+                    if (numInput > 0 && numInput <= 9)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong Number, Try again: ");
+                    }
+                }
+
+                if (board[numInput - 1] == numInput.ToString())
+                {
+                    board[numInput - 1] = turn;
+                    turn = "O";
+                }
+
+                if (turn == "O")
+                {
+                    MinMax.PlayBestMove(board);
+                    PrintBoard();
+                    turn = "X";
+                }
+                else
+                {
+                    Console.WriteLine("Number is already taken try again: ");
+                }
+
+                if (WinnerIsKnown()) break;
+
+            }
+        }
+
+        public static string CheckWinner()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                string line = string.Empty;
+
+                switch (i)
+                {
+                    case 0:
+                        line = board[0] + board[1] + board[2];
+                        break;
+                    case 1:
+                        line = board[3] + board[4] + board[5];
+                        break;
+                    case 2:
+                        line = board[6] + board[7] + board[8];
+                        break;
+                    case 3:
+                        line = board[0] + board[3] + board[6];
+                        break;
+                    case 4:
+                        line = board[1] + board[4] + board[7];
+                        break;
+                    case 5:
+                        line = board[2] + board[5] + board[8];
+                        break;
+                    case 6:
+                        line = board[0] + board[4] + board[8];
+                        break;
+                    case 7:
+                        line = board[2] + board[4] + board[6];
+                        break;
+                }
+
+                //თუ X-მ გაიმარჯვა
+                if (line == "XXX")
+                    return "X";
+                //თუ O-მ გაიმარჯვა
+                else if (line == "OOO")
+                    return "O";
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                if (!board.Contains((i + 1).ToString()))
+                {
+                    if (i == 8)
+                        return "Draw";
+                }
+                else break;
+            }
+            return null;
+        }
+
+        public static bool WinnerIsKnown()
+        {
+            string winner = CheckWinner();
+            if (winner != null)
+            {
+                if (winner == "O")
+                {
+                    Console.Write("You lost, ");
+                }
+                else if (winner == "Draw")
+                {
+                    Console.Write("Draw, ");
+                }
+                else Console.WriteLine("You won, ");
+                Console.WriteLine("Game Over.");
+
+                return true;
+            }
+            return false;
+        }
+
+        public static void PrintBoard()
+        {
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine($"|---|---|---|");
+            Console.WriteLine($"| {board[0]} | {board[1]} | {board[2]} |");
+            Console.WriteLine($"|-----------|");
+            Console.WriteLine($"| {board[3]} | {board[4]} | {board[5]} |");
+            Console.WriteLine($"|-----------|");
+            Console.WriteLine($"| {board[6]} | {board[7]} | {board[8]} |");
+            Console.WriteLine($"|---|---|---|");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
---
+++ Tic-Tac-Toe_MinMax/MinMax.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Tic_Tac_Toe_MinMax
+{
+    public static class MinMax
+    {
+        private static int MAX_DEPTH = 6;
+
+        public static void PlayBestMove(string[] board)
+        {
+            int bestMove = -1;
+            int bestValue = Int32.MinValue;
+
+            for (int i = 0; i < board.Length; i++)
+            {
+                if (board[i] != "X" && board[i] != "O")
+                {
+                    var tmp = board[i];
+                    board[i] = "O";
+                    int moveValue = MiniMax(board, MAX_DEPTH, false);
+
+                    board[i] = tmp;
+
+                    if (moveValue > bestValue)
+                    {
+                        bestMove = i;
+                        bestValue = moveValue;
+                    }
+                }
+            }
+
+            if (bestMove == -1)
+            {
+                return;
+            }
+
+            board[bestMove] = "O";
+        }
+
+        public static int MiniMax(string[] board, int depth, bool isMax)
+        {
+            string boardVal = GameStart.CheckWinner();
+            if (boardVal != null)
+            {
+                if (boardVal == "O")
+                    return 10;
+                else if (boardVal == "X")
+                    return -10;
+                else if (depth == 0 || boardVal == "Draw")
+                    return 0;
+            }
+
+
+            // Maximising player, find the maximum attainable value.
+            if (isMax)
+            {
+                int highestVal = Int32.MinValue;
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (board[i] != "X" && board[i] != "O")
+                    {
+                        board[i] = "O";
+                        highestVal = Math.Max(highestVal, MiniMax(board, depth - 1, false));
+                        board[i] = (i + 1).ToString();
+                    }
+                }
+                return highestVal;
+                // Minimising player, find the minimum attainable value
+            }
+            else
+            {
+                int lowestVal = Int32.MaxValue;
+                for (int i = 0; i < board.Length; i++)
+                {
+                    if (board[i] != "X" && board[i] != "O")
+                    {
+                        board[i] = "X";
+                        lowestVal = Math.Min(lowestVal, MiniMax(board, depth - 1, true));
+                        board[i] = (i + 1).ToString();
+                    }
+                }
+                return lowestVal;
+            }
+        }
+    }
+}
